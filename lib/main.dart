43a95682c1bb54cc8d12c4e97c41039493a23fbb
[Takuto.A] <[kuroitu9@gmail.com]>
2020-06-03 00:46:16 +0900

    リストを追加しリスト一覧に表示する機能を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(TodoApp());
@@ -19,46 +20,47 @@ class TodoApp extends StatelessWidget {
 }
 
 //リスト一覧画面用Widget
-class TodoListPage extends StatelessWidget {
+class TodoListPage extends StatefulWidget {
+  @override
+  _TodoListPageState createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  // Todoリストのデータを扱うStateウィジェット
+  List<String> todoList = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('タスク一覧'),
       ),
-      body: ListView(
-        children: <Widget>[
-          Card(
+      body: ListView.builder(
+        itemCount: todoList.length, //todoリストの個数を保持
+        itemBuilder: (context, index) {
+          //todoリストの文字列配列を元にCardを作成
+          return Card(
             child: ListTile(
-              title: Text('課題1'),
+              title: Text(todoList[index]),
             ),
-          ),
-          Card(
-            child: ListTile(
-              title: Text('課題2'),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: Text('課題3'),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: Text('課題4'),
-            ),
-          ),
-        ],
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.library_add),
-        onPressed: () {
-          Navigator.of(context).push(
+        onPressed: () async {
+          final newListText = await Navigator.of(context).push(
             //pushで新規画面に遷移
             MaterialPageRoute(builder: (context) {
               return TodoAddPage();
             }),
           );
+          if (newListText != null) {
+            //キャンセルすると newListTextが nullになることに注意
+            setState(() {
+              todoList.add(newListText);
+            });
+          }
         },
       ),
     );
@@ -66,16 +68,63 @@ class TodoListPage extends StatelessWidget {
 }
 
 //リスト追加画面用Widget
-class TodoAddPage extends StatelessWidget {
+class TodoAddPage extends StatefulWidget {
+  @override
+  _TodoAddPageState createState() => _TodoAddPageState();
+}
+
+class _TodoAddPageState extends State<TodoAddPage> {
+  //入力されたテキストをデータとして持つ
+  String _text = '';
+
+  //データを元に表示するWidget
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: FlatButton(
-          onPressed: () {
-            Navigator.of(context).pop(); //pop()で前の画面に戻る
-          },
-          child: Text('リスト追加画面(タップで戻る)'),
+      appBar: AppBar(
+        title: Text("タスクを追加"),
+        leading: IconButton(
+            icon: new Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.of(context).pop();
+            }),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(64),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              //入力されたテキストの値を受け取る(valueが入力されたテキスト)
+              onChanged: (String value) {
+                setState(() {
+                  //データを更新
+                  _text = value;
+                });
+              },
+            ),
+            Container(
+              width: double.infinity, //画面幅いっぱいにContainerを広げる
+              //リスト追加ボタン
+              child: RaisedButton(
+                color: Colors.blue,
+                onPressed: () {
+                  Navigator.of(context).pop(_text); //popの引数から前の画面にデータを渡せる
+                },
+                child: Text("追加", style: TextStyle(color: Colors.white)),
+              ),
+            ),
+            Container(
+              width: double.infinity, //画面幅いっぱいにContainerを広げる
+              //キャンセルボタン(押すとリスト一覧画面に戻る)
+              child: FlatButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text("キャンセル"),
+              ),
+            ),
+          ],
         ),
       ),
     );
